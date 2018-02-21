d059a81da0d51db5a3f694540522fabbdc443f3a
imar26 <aadeshranderia26@gmail.com>
2018-02-21 01:46:29 -0500

    port config variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-var PORT = 5000;
+var PORT = process.env.PORT || 5000;
 
 connect().use(serveStatic(__dirname)).listen(PORT, function () {
     console.log('Server running on ' + PORT + '...');
